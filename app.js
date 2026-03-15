5f807e1fd4ddc1b81061561d2ee95defa591c336
PruthviDev-7 <your-email@example.com>
2026-03-15 11:15:19 +0530

    made loder successfuly

---
+++ app.js
@@ -0,0 +1,43 @@
+var tl = gsap.timeline();
+tl.from(".line h1", {
+  y: 150,
+  opacity: 0,
+  stagger: 0.25,
+  duration: 0.6,
+  delay: 0.5,
+});
+tl.from("#line1-part1",{
+  opacity: 0,
+  onStart: function () {
+    var h5timer = document.querySelector("#line1-part1 h5");
+    var count = 0;
+    var int = setInterval(() => {
+      if (count < 100) {
+        count++;
+        h5timer.innerHTML = count;
+      } else {
+        clearInterval(int);
+      }
+    }, 35);
+  },
+});
+tl.to(".line h2",{
+   animationName: "anme",
+   opacity: 1,
+  
+})
+tl.to("#loder", {
+  opacity: 0,
+  duration: 0.2,
+  delay: 4,
+});
+tl.from("#page1",{
+  y: 1600,
+  opacity: 0,
+  delay: 0.2,
+  duration: 0.5,
+  ease:Power4
+})  
+tl.to("#loder",{
+  display: "none",
+})
